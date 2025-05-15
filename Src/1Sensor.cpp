ac8384c81b061e0c58b6232e9cc2956f54232042
5CD322B26V\12302093 <12302093@student.pxl.be>
2025-05-15 13:05:57 +0200

    Modified code for 1 sensor with WiFi

--- Src/1Sensor.cpp
+++ Src/1Sensor.cpp
@@ -1,4 +1,16 @@
 #include <SDI12.h>
+#include <WiFi.h>
+#include <WiFiUDP.h>
+
+// Replace with your network credentials
+const char* ssid = "504F94A12106";      // Your WiFi SSID
+const char* password = "";  // Your WiFi Password
+
+// IP and port for sending and receiving UDP packets
+const char* udpIP = "192.168.1.77";  // IP to send UDP packets to (Miniserver IP)
+const int udpPort = 50001;           // Port to send UDP packets to
+
+WiFiUDP udp;  // Initialize WiFiUDP object
 
 #define DATA_PIN 17 // GPIO pin verbonden met de SDI-12 datalijn
 
@@ -10,9 +22,31 @@ void setup() {
   mySDI12.begin();
   delay(500);
   Serial.println("SDI-12 communicatie gestart");
+
+  WiFi.begin(ssid, password);  // Start connecting to WiFi
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println("WiFi connected!");
+
+    // Start listening for UDP packets on the specified port
+    udp.begin(udpPort);  // Bind to UDP port
+  
+  
 }
 
+
+
 void loop() {
+  WiFiUDP udp;
+
+  const char * udpAddress = "192.168.1.77";
+  const int udpPort = 50001;
+
   mySDI12.sendCommand("0M!"); // Start meting op adres 0
   delay(1500); // Wacht op meting
 
@@ -75,6 +109,13 @@ void loop() {
   Serial.print(vwc, 2);
   Serial.println(" µS/cm");
   Serial.println("--------------------------------------");
+
+  
+
+
+  udp.beginPacket(udpAddress,udpPort);
+  udp.printf("VWC: %f, Temp: %f, EC: %f", ec, temp, vwc);
+  udp.endPacket();
 
   delay(1000); // Wacht 10 seconden voor de volgende meting
 }
